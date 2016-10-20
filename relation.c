6f72cc662a5928ff4c4a276b39bc0ecef152bf68
Dipak  Gautam <gautamdeepak80@gmail.com>
2016-10-20 12:15:24 +0545

    use of relation operator

---
+++ relation.c
@@ -0,0 +1,43 @@
+/*
+Student Name :Dipak Gautam
+Subject:Programming fundamentals 
+lab no:04
+Date:20 oct,2016
+*/
+
+#include<stdio.h>
+//#include<conio.h>
+
+int main ()
+{
+int a,b;
+printf("Enter one value(a)\n");
+scanf("%d",&a);
+printf("Enter second value (b)\n");
+scanf("%d",&b);
+
+
+printf("Use of '==' operator : %d == %d is %d\n",a,b,a==b);
+
+
+printf("Use of '>' operator :%d > %d is %d\n",a,b,a>b);
+
+
+printf("Use of '<' operator :%d < %d is %d\n",a,b,a<b);
+ 
+ 
+
+printf("Use of '!=' operator : %d != %d is %d\n",a,b,a!=b);
+
+
+printf("Use of '>=' operator : %d >= %d is %d\n",a,b,a>=b);
+
+
+printf("Use of '<=' operator : %d <= %d is %d\n",a,b,a<=b);
+
+return 0;
+}
+
+
+
+
