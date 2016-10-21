dd0cbf8af931f06c98147de52d431ed07d3fa25d
Dipak  Gautam <gautamdeepak80@gmail.com>
2016-10-21 11:32:18 +0545

    odd or even

---
+++ oddeven.c
@@ -0,0 +1,27 @@
+/*
+Student Name:Dipak Gautam
+Subject:Programmingf Fndamentals
+Program:Program to print Odd and Even nos.
+Lab no:05
+Date:21 oct 2016
+*/
+#include<stdio.h>
+//#include<conio.h>
+
+int main()
+{
+	int a;
+	printf("Enter a number:\n");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("%d is even number \n",a);
+	}
+	else 
+	{
+		printf("%d is  odd number\n",a);
+	}
+	
+	
+	return 0;
+     }
