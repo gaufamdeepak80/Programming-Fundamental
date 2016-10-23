1236f3c1a2b216dfcc8ec95497fa48a1229f3fe3
Dipak  Gautam <gautamdeepak80@gmail.com>
2016-10-23 11:11:34 +0545

    menu calculator

--- calculator.c
+++ calculator.c
@@ -10,11 +10,13 @@ Date:23 oct,2016
 
 int main()
 {
-char op;
+int op;
 float a,b,r;
-printf("Enter a operation(+,-,*,/): \n");
+printf("SIMPLE CALCULATOR\n");
 
-scanf("%c",&op);
+printf("Enter as command\n 1 for addition\n 2 for subtraction\n 3 for multiplication\n 4 for division: \n");
+
+scanf("%d",&op);
 printf("Enter any number:  \n");;
 scanf("%f",&a);
 printf("Enter another value:  \n");
@@ -23,19 +25,19 @@ scanf("%f",&b);
 //op=getchar();
 switch(op)
 {
-case'+':{
+case 1:{
 r=a+b;
 printf("Sum of two number is %f",r);}
 break;
-case'-':{
+case 2:{
 r=a-b;
 printf("difference of two number is %f",r);}
 break;
-case'*':{
+case 3:{
 r=a*b;
 printf("product of two number is %f",r);}
 break;
-case'/':{
+case 4:{
 r=a/b;
 printf("division of two number is  %f",r);}
 break;
