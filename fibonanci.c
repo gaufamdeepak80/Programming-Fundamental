83f16bd78b5819bdb25f468a317fee2facec9aff
Dipak  Gautam <gautamdeepak80@gmail.com>
2016-12-27 10:28:26 +0545

    fibonanci series

---
+++ fibonanci.c
@@ -0,0 +1,24 @@
+/*
+student Name:Dipak Gautam
+Subject:programming fundamentals
+program:Fibonacci series
+Lab no:21
+date:27 dec,2016
+*/
+#include<stdio.h>
+int main()
+{
+    int b=1,c,i,a=1;
+    for(i=1;i<=10;i++)
+    {
+      c=a+b;
+      printf("%d\t",a);
+      a=b;
+      b=c;
+
+
+
+
+
+    }
+}
