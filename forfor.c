f8d1ebf10b5b1c9243350b27e1ee305003cbabe0
Dipak  Gautam <gautamdeepak80@gmail.com>
2016-12-29 10:29:47 +0545

    pattern1

---
+++ forfor.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	for(y='A';y<'K';y++){
+		
+	  
+	for(x=1;x<=10;x++){
+		
+	 
+	   printf("%c%d\t",y,x);
+	   
+	}
+	printf("\n");
+	}      
+	   return 0;
+}
