793f75cc7098d9d4424d4e6565d445a604804a20
Dipak  Gautam <gautamdeepak80@gmail.com>
2017-01-05 10:19:13 +0545

    fibonacci    using    do    while

---
+++ fibonacciusingdowhile.c
@@ -0,0 +1,29 @@
+/*
+Student Name:Dipak Gautam
+Subject:Programming Fundamentals
+Program:Fibonaci serries
+LAb no:24
+Date:3rd Jan
+*/
+#include<stdio.h>
+
+int main()
+{
+    int a=1,b=1,i,n,c;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    i=1;
+
+     do
+     {
+        c=a+b;
+        printf("%d\t",a);
+        a=b;
+        b=c;
+        i++;
+    }
+       while(i<=n);
+
+
+}
+
