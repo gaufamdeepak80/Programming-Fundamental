3a4e537ed2fdffa6c6029c4b2fab3753392d7fde
Dipak  Gautam <gautamdeepak80@gmail.com>
2017-01-08 11:17:33 +0545

    alpabet or not

---
+++ alphabet.c
@@ -0,0 +1,19 @@
+/*
+student name:Dipak Gautam
+subject:Programming fundamentals
+Program:Leap Year
+lab no:25
+Date:29 dec,2016
+*/
+#include<stdio.h>
+int main()
+{
+    char ch,a;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    (ch>='A' && ch<='Z' || ch>='a' && ch<='z') ? printf("%c is an Alphabet",ch):printf(" %c is not an Alphabet",ch);
+
+
+printf("Enter a character:");
+    scanf("%c",&a);
+}
